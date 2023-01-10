eb14bf2b3231caad8227e7d5f49fe907e00373a7
carrotpai <55906326+carrotpai@users.noreply.github.com>
2023-01-10 14:10:05 +0800

    working with web3 api

--- constants/abi.js
+++ constants/abi.js
@@ -1,78 +1,52 @@
 module.exports = {
     abi: [{
-            inputs: [{
-                    internalType: "string",
-                    name: "_name",
-                    type: "string",
-                },
-                {
-                    internalType: "uint256",
-                    name: "_favoriteNumber",
-                    type: "uint256",
-                },
-            ],
-            name: "addPerson",
-            outputs: [],
-            stateMutability: "nonpayable",
-            type: "function",
+            "inputs": [],
+            "stateMutability": "nonpayable",
+            "type": "constructor"
         },
         {
-            inputs: [{
-                internalType: "string",
-                name: "",
-                type: "string",
-            }, ],
-            name: "nameToFavoriteNumber",
-            outputs: [{
-                internalType: "uint256",
-                name: "",
-                type: "uint256",
-            }, ],
-            stateMutability: "view",
-            type: "function",
+            "inputs": [{
+                "internalType": "uint256",
+                "name": "_rating",
+                "type": "uint256"
+            }],
+            "name": "balanceAccrual",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
         },
         {
-            inputs: [{
-                internalType: "uint256",
-                name: "",
-                type: "uint256",
-            }, ],
-            name: "people",
-            outputs: [{
-                    internalType: "uint256",
-                    name: "favoriteNumber",
-                    type: "uint256",
-                },
-                {
-                    internalType: "string",
-                    name: "name",
-                    type: "string",
-                },
-            ],
-            stateMutability: "view",
-            type: "function",
+            "inputs": [],
+            "name": "baseSupply",
+            "outputs": [{
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }],
+            "stateMutability": "view",
+            "type": "function"
         },
         {
-            inputs: [],
-            name: "retrieve",
-            outputs: [{
-                internalType: "uint256",
-                name: "",
-                type: "uint256",
-            }, ],
-            stateMutability: "view",
-            type: "function",
+            "inputs": [],
+            "name": "getBalance",
+            "outputs": [{
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }],
+            "stateMutability": "view",
+            "type": "function"
         },
         {
-            inputs: [{
-                internalType: "uint256",
-                name: "_favoriteNumber",
-                type: "uint256",
-            }, ],
-            name: "store",
-            outputs: [],
-            stateMutability: "nonpayable",
-            type: "function",
-        },
+            "inputs": [],
+            "name": "totalsupply",
+            "outputs": [{
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }],
+            "stateMutability": "view",
+            "type": "function"
+        }
     ],
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,28 +2,48 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
-import {useMoralis, useWeb3Contract} from "react-moralis";
+import {useMoralis, useWeb3Contract, useWeb3ExecuteFunction} from "react-moralis";
 import { useState, useEffect } from "react";
 import { abi } from "../constants/abi";
+import { BigNumber, ethers } from 'ethers';
 
 const inter = Inter({ subsets: ['latin'] })
+
+interface contractResponse {
+  data: BigNumber;
+}
+
+function isContractResponse(obj: unknown): obj is contractResponse {
+  return (
+    typeof obj === 'object' && obj !== null && 'data' in obj
+  );
+}
 
 
 export default function Home() {
 
 
   const [hasMetamask, setHasMetamask] = useState(false);
-  const { enableWeb3, isWeb3Enabled } = useMoralis();
+  const { enableWeb3, isWeb3Enabled, account } = useMoralis();
 
-  const {data, error, runContractFunction, isFetching, isLoading} = 
-    useWeb3Contract({
-      abi: abi,
-      contractAddress: "xxx",
-      functionName: "store",
-      params: {
-        _favoriteNumber: 42,
-      },
-    });
+  async function getData(){
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract("0x5FbDB2315678afecb367f032d93F642f64180aa3", abi, signer);
+    const balance: BigNumber = await contract.getBalance()
+    return Number(balance.toHexString());
+  }
+  
+
+  const {data, error, fetch, isFetching, isLoading} = useWeb3ExecuteFunction({
+    abi: abi,
+    contractAddress: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+    functionName: "balanceAccrual",
+    params: {
+      _rating: 1,
+    },
+  });
+
 
   useEffect(()=>{
     if (typeof window.ethereum !== "undefined"){
@@ -45,7 +65,7 @@ export default function Home() {
       )}
 
       {isWeb3Enabled ? (
-        <button onClick={()=> runContractFunction()}></button>
+        <button onClick={() => fetch()}>Execute</button>
       ) : (
         ""
       )}
